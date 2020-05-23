df4b2e4c078a1a51b266308c53352f28b8f3a492
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2020-05-23 12:34:20 -0500

    more unit tests added

--- app.js
+++ app.js
@@ -1,18 +1,84 @@
 const express = require('express');
 const app = express();
 const cal = require('./calculator');
+const CodeBreaker = require('./codebreaker');
+const cb = new CodeBreaker('0123');
 
-app.get('/add', (req, res) => {
-  res.json({
-    result: cal.add(parseInt(req.query.value1), parseInt(req.query.value2)),
-  });
+app.get('/calculator/add', async (req, res) => {
+  try {
+    const value1 = parseInt(req.query.value1);
+    const value2 = parseInt(req.query.value2);
+
+    const sub = await cal.add(value1, value2);
+    res.status(200).json({
+      result: sub,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
-app.get('/substract', (req, res) => {
-  res.json({
-    result: cal.substract(
-      parseInt(req.query.value1),
-      parseInt(req.query.value2)
-    ),
-  });
+app.get('/calculator/sub', async (req, res) => {
+  try {
+    const value1 = parseInt(req.query.value1);
+    const value2 = parseInt(req.query.value2);
+
+    const sub = await cal.substract(value1, value2);
+    res.status(200).json({
+      result: sub,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
+
+app.get('/codebreaker/isPositioned', async (req, res) => {
+  try {
+    const value = parseInt(req.query.value);
+    const position = parseInt(req.query.position);
+
+    const isPositioned = await cb.isPositioned(value, position);
+    res.status(200).json({
+      result: isPositioned,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/codebreaker/itExist', async (req, res) => {
+  try {
+    const value = parseInt(req.query.value);
+
+    const itExist = await cb.itExist(value);
+    res.status(200).json({
+      result: itExist,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/codebreaker/guest', async (req, res) => {
+  try {
+    const value = req.query.value;
+
+    const guest = await cb.guest(value);
+    res.status(200).json({
+      result: guest,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+app.get('/codebreaker/setsecret', async (req, res) => {
+  try {
+    const value = req.query.value;
+
+    await cb.setSecret(value);
+    res.status(200).json();
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,19 +1,140 @@
 const app = require('./app');
 const supertest = require('supertest');
-const request = supertest(app);
+const request = supertest(app); //Arange
 
-test('gets add endpoint', async (done) => {
-  const response = await request.get('/add?value1=2&value2=5');
-  expect(response.status).toBe(200);
-  expect(response.type).toBe('application/json');
-  expect(response.body.result).toBe(7);
-  done();
+describe('Testing Calculator', () => {
+  test('Get the add Endpoint', async (done) => {
+    //Act
+    const response = await request.get('/calculator/add?value1=1&value2=3');
+
+    //asset
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body.result).toBe(4);
+    done();
+  });
+
+  test('Get the subtract Endpoint', async (done) => {
+    //Act
+    const response = await request.get('/calculator/sub?value1=1&value2=3');
+
+    //asset
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body.result).toBe(-2);
+    done();
+  });
 });
 
-test('gets substract endpoint', async (done) => {
-  const response = await request.get('/substract?value1=5&value2=2');
-  expect(response.status).toBe(200);
-  expect(response.type).toBe('application/json');
-  expect(response.body.result).toBe(3);
-  done();
+describe('Testing CodeBreaker', () => {
+  describe('Testing isPositioned', () => {
+    test('Get the isPositioned Endpoint', async (done) => {
+      //Act
+      const response = await request.get(
+        '/codebreaker/isPositioned?value=0&position=0'
+      );
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe(true);
+      done();
+    });
+
+    test('Get the isPositioned Endpoint', async (done) => {
+      //Act
+      const response = await request.get(
+        '/codebreaker/isPositioned?value=8&position=2'
+      );
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe(false);
+      done();
+    });
+  });
+
+  describe('Testing itExist', () => {
+    test('Get the itExist Endpoint', async (done) => {
+      //Act
+      const response = await request.get('/codebreaker/itExist?value=1');
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe(true);
+      done();
+    });
+
+    test('Get the itExist Endpoint', async (done) => {
+      //Act
+      const response = await request.get('/codebreaker/itExist?value=8');
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe(false);
+      done();
+    });
+  });
+
+  describe('Testing guest', () => {
+    test('Get the guest Endpoint', async (done) => {
+      //Act
+      const response = await request.get('/codebreaker/guest?value=0123');
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe('XXXX');
+      done();
+    });
+    test('Get the guest Endpoint', async (done) => {
+      //Act
+      const response = await request.get('/codebreaker/guest?value=6789');
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe('');
+      done();
+    });
+    test('Get the guest Endpoint', async (done) => {
+      //Act
+      const response = await request.get('/codebreaker/guest?value=1032');
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe('____');
+      done();
+    });
+    test('Get the guest Endpoint', async (done) => {
+      //Act
+      const response = await request.get('/codebreaker/guest?value=0513');
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBe('XX_');
+      done();
+    });
+  });
+  describe('Testing setSecret', () => {
+    test('Get the setSecret Endpoint', async (done) => {
+      const newSecret = '5678';
+
+      //Act
+      const response = await request.get(
+        `/codebreaker/setsecret?value=${newSecret}`
+      );
+
+      //asset
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.body.result).toBeUndefined();
+      done();
+    });
+  });
 });
